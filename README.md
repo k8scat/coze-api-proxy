529475f58c51a95f8735a559724fb80fc78fa550
K8sCat <k8scat@gmail.com>
2024-05-14 10:52:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# coze-api-proxy
+
+Coze API proxy on Netlify Edge
+
+## Deploy
+
+### Deploy With Netlify
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/k8scat/coze-api-proxy)
---
+++ netlify/functions/proxy.ts
@@ -0,0 +1,68 @@
+import { Context } from "@netlify/edge-functions";
+
+const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
+  const picked = new Headers();
+  for (const key of headers.keys()) {
+    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
+      const value = headers.get(key);
+      if (typeof value === "string") {
+        picked.set(key, value);
+      }
+    }
+  }
+  return picked;
+};
+
+const CORS_HEADERS: Record<string, string> = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "*",
+  "access-control-allow-headers": "*",
+};
+
+export default async (request: Request, context: Context) => {
+
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      headers: CORS_HEADERS,
+    });
+  }
+
+  const { pathname, searchParams } = new URL(request.url);
+  if(pathname === "/") {
+    return new Response(null, {
+      headers: {
+        ...CORS_HEADERS,
+        "content-type": "text/html",
+        "location": "https://api.0pena1.com/",
+      },
+      status: 301,
+    });
+  }
+
+  const url = new URL(pathname, "https://api.coze.com");
+  searchParams.delete("_path");
+
+  searchParams.forEach((value, key) => {
+    url.searchParams.append(key, value);
+  });
+
+  const headers = pickHeaders(request.headers, ["content-type", "host", "authorization", "accept", "connection"]);
+
+  const response = await fetch(url, {
+    body: request.body,
+    method: request.method,
+    duplex: 'half',
+    headers,
+  });
+
+  const responseHeaders = {
+    ...CORS_HEADERS,
+    ...Object.fromEntries(response.headers),
+    "content-encoding": null
+  };
+
+  return new Response(response.body, {
+    headers: responseHeaders,
+    status: response.status
+  });
+};
